7c54516e2e935af536a5ff0215b1774b69fccefe
orcinusorca1331@gmail.com <jacobmorris@Jacobs-MacBook-Pro-4.local>
2023-08-01 00:13:50 -0600

    add all my files

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import 'react-native-gesture-handler';
+import React from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+
+import Navigation from './src/navigation';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={{flex: 1}}>
+        <Navigation />
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
---
+++ data/posts.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    id: 'p1',
+    videoUri:
+      'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+    user: {
+      id: 'u1',
+      username: 'tonyrobbins',
+      imageUri:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzanDpuAsVs2m-VZe2kmrKBN64MlAYTuGfsg&usqp=CAU',
+    },
+    description: 'In search of external validation #lifting',
+    song: 'Jimi Hendrix - Killing Floor',
+    songImage:
+      'https://upload.wikimedia.org/wikipedia/en/0/02/Jimimonterey.jpg',
+    likes: 13,
+    comments: '12.9K',
+    shares: '131.9K',
+  },
+
+  {
+    id: 'p2',
+    videoUri:
+      'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+    user: {
+      id: 'u1',
+      username: 'tonyrobbins',
+      imageUri:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzanDpuAsVs2m-VZe2kmrKBN64MlAYTuGfsg&usqp=CAU',
+    },
+    description: 'In search of external validation #lifting',
+    song: 'Jimi Hendrix - Killing Floor',
+    songImage:
+      'https://upload.wikimedia.org/wikipedia/en/0/02/Jimimonterey.jpg',
+    likes: 13,
+    comments: '12.9K',
+    shares: '131.9K',
+  },
+
+  {
+    id: 'p3',
+    videoUri:
+      'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+    user: {
+      id: 'u1',
+      username: 'tonyrobbins',
+      imageUri:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzanDpuAsVs2m-VZe2kmrKBN64MlAYTuGfsg&usqp=CAU',
+    },
+    description: 'In search of external validation #lifting',
+    song: 'Jimi Hendrix - Killing Floor',
+    songImage:
+      'https://upload.wikimedia.org/wikipedia/en/0/02/Jimimonterey.jpg',
+    likes: 13,
+    comments: '12.9K',
+    shares: '131.9K',
+  },
+];
